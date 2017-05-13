2b933d3f89b0ca4a53dc13d9a549a5e42f862aa4
邓斌 Bean <dengbin@gznb.com>
2017-05-13 23:50:04 +0800

    Revert "对于Android同样计算dpr"
    This reverts commit a03ca47ef03394367d45f1cacd2e88facddffead.

--- src/flexible.js
+++ src/flexible.js
@@ -33,12 +33,21 @@
     }
 
     if (!dpr && !scale) {
+        var isAndroid = win.navigator.appVersion.match(/android/gi);
+        var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+        var isIPad = win.navigator.appVersion.match(/ipad/gi);
         var devicePixelRatio = win.devicePixelRatio;
-        if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
-            dpr = 3;
-        } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
-            dpr = 2;
+        if (isIPhone || isIPad) {
+            // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+                dpr = 3;
+            } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
+                dpr = 2;
+            } else {
+                dpr = 1;
+            }
         } else {
+            // 其他设备下，仍旧使用1倍的方案
             dpr = 1;
         }
         scale = 1 / dpr;
